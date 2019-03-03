94beefcfdd39ace3775bd61aaa133ea2abb82fc1
NataChasovskikh <flosalnatali@gmail.com>
2019-03-03 17:54:35 +0200

    added homework4

--- JSHomeWork/HomeWork4/js/HomeWork4.js
+++ JSHomeWork/HomeWork4/js/HomeWork4.js
@@ -45,8 +45,40 @@ const products = {
  
  function Cashier(name, productDatabase) {
    // 🔔 не забывайте о this при обращении к свойствам и методам будущего объекта
- }
+    this.name = name;
+    this.productDatabase = productDatabase; 
+    this.customerMoney = 0;
+    this.getCustomerMoney = function(value) {
+    this.customerMoney = value;
+    }
  
+  this.countTotalPrice = function(order) {
+    let sum = 0;
+    for(kay in order) {
+      sum += order[kay] * productDatabase[kay];      
+    }
+    return sum;
+  }
+  this.countChange = function (totalPrice) {
+    console.log(totalPrice >= this.customerMoney);    
+    
+    if (this.customerMoney >= totalPrice){
+      let res = this.customerMoney - totalPrice;
+      return res;
+    }else {
+      return null;
+    }
+  }
+  this.onSuccess = function(change) {
+    console.log(`Спасибо за покупку, ваша сдача ${change}!`); 
+  }
+  this.onError = function() {
+    console.log('Очень жаль, вам не хватает денег на покупки');
+  }
+  this.reset = function() {
+    this.customerMoney = 0;
+  }
+}
  /* Заказ пользователя хранится в виде объекта следующего формата. "имя-продукта":"количество-единиц" */
  const order = {
    bread: 2,
