6c5ebf363a7d351a83e2c8a205f7aeae3c84d6de
NataChasovskikh <flosalnatali@gmail.com>
2019-03-25 13:35:38 +0200

    homework8

--- JSHomeWork/HomeWork8/js/HomeWork8.js
+++ JSHomeWork/HomeWork8/js/HomeWork8.js
@@ -53,142 +53,60 @@
   Замените пути на соотвествующие вашим, или назовите изображения аналогично.
 */
 
-// const galleryItems = [
-//    { preview: 'img/preview-1.jpeg', fullview: 'img/fullview-1.jpeg', alt: "alt text 1" },
-//    { preview: 'img/preview-2.jpeg', fullview: 'img/fullview-2.jpeg', alt: "alt text 2" },
-//    { preview: 'img/preview-3.jpeg', fullview: 'img/fullview-3.jpeg', alt: "alt text 3" },
-//    { preview: 'img/preview-4.jpeg', fullview: 'img/fullview-4.jpeg', alt: "alt text 4" },
-//    { preview: 'img/preview-5.jpeg', fullview: 'img/fullview-5.jpeg', alt: "alt text 5" },
-//    { preview: 'img/preview-6.jpeg', fullview: 'img/fullview-6.jpeg', alt: "alt text 6" },
-//  ];
+const galleryItems = [
+   { preview: '../../img/preview1.jpg', fullview: '../../img/fullview1.jpg', alt: "alt text 1" },
+   { preview: '../../img/preview2.jpg', fullview: '../../img/fullview2.jpg', alt: "alt text 2" },
+   { preview: '../../img/preview3.jpg', fullview: '../../img/fullview3.jpg', alt: "alt text 3" },
+   { preview: '../../img/preview4.jpg', fullview: '../../img/fullview4.jpg', alt: "alt text 4" },
+   { preview: '../../img/preview5.jpg', fullview: '../../img/fullview5.jpg', alt: "alt text 5" },
+   { preview: '../../img/preview6.jpg', fullview: '../../img/fullview6.jpg', alt: "alt text 6" },
+ ];
  
  
-//  const container = document.querySelector('.js-image-gallery');
- 
-//  let createFullview = () => {
-//    let fullviewEl = document.createElement('div');
-//    fullviewEl.classList.add('fullviewEl');
-//    let fullviewImg = document.createElement('img');
-//    fullviewImg.classList.add('fullviewImg');
- 
-//    fullviewImg.src = galleryItems[0].fullview;
- 
-//    fullviewEl.appendChild(fullviewImg);
-//    container.appendChild(fullviewEl);
-//  };
- 
-//  createFullview();
- 
-//  let createPreview = () => {
- 
-//    let ul = document.createElement('ul');
-//    ul.classList.add('previewElements');
-//    container.appendChild(ul);
- 
-//    galleryItems.forEach(el => {
-//      let li = document.createElement('li');
-//      let img = document.createElement('img');
-//      img.setAttribute("data-fullview", el.fullview);
-//      img.setAttribute("alt", el.alt);
-//      img.src = el.preview;
-//      li.appendChild(img);
-//      ul.appendChild(li);
-//    });
- 
-//  };
-//  createPreview();
- 
-//  const preview = document.querySelector('.previewElements');
- 
-//  const fullview = document.querySelector('.fullviewImg');
- 
-//  preview.addEventListener('click', event => {
-//    const nodeName = event.target.nodeName;
-//    if (nodeName !== 'IMG') return;
-//    const fullviewSrc = event.target.getAttribute('data-fullview');
-//    fullview.src = fullviewSrc;
-//  });
-///===========================
-// const imgGallery = document.querySelector(".js-image-gallery");
-// const fullview = document.createElement("div");
-// imgGallery.appendChild(fullview);
-// fullview.classList.add("fullview");
-// const fullviewMainImg = document.createElement("img");
-// fullview.appendChild(fullviewMainImg);
-// fullviewMainImg.setAttribute("src", galleryItems[0].fullview);
-// fullviewMainImg.setAttribute("alt", "alt text 1");
-// const preview = document.createElement("ul");
-// imgGallery.appendChild(preview);
-// preview.classList.add("preview");
+ const gallery = document.querySelector('.js-image-gallery');
+ const fullview = document.createElement('div');
+ gallery.appendChild(fullview);
+ fullview.classList.add('fullview');
+ const fullviewImg = document.createElement("img");
+ fullview.appendChild(fullviewImg);
+ fullviewImg.setAttribute("src", galleryItems[0].fullview);
+ fullviewImg.setAttribute("alt", "alt text 1");
+ const preview = document.createElement("ul");
+ gallery.appendChild(preview);
+ preview.classList.add("preview");
+ const previewGallery = document.querySelector(".preview");
 
-// function createImg() {
-//   galleryItems.forEach(function(item) {
-//     const galleryConteiner = document.createElement("li");
-//     preview.appendChild(galleryConteiner);
-//     const galleryImg = document.createElement("img");
-//     galleryConteiner.appendChild(galleryImg);
-//     galleryImg.setAttribute("src", item.preview);
-//     galleryImg.setAttribute("data-fullview", item.fullview);
-//     galleryImg.setAttribute("alt", item.alt);
-//   });
-// }
-// createImg();
-
-// const previewGallery = document.querySelector(".preview");
-
-// function changeFullview(event) {
-//   const target = event.target;
-//   if (target.nodeName !== "IMG") return;
-//   findPreview(target);
-//   lightPreview(target);
-// }
-
-// function findPreview(elem) {
-//   fullviewMainImg.src = elem.dataset.fullview;
-// }
-
-// function lightPreview(elem) {
-//   const currentElem = document.querySelector(".newClass");
-//   if (currentElem) {
-//     currentElem.classList.remove("newClass");
-//   }
-//   elem.classList.add("newClass");
-// }
-
-// previewGallery.addEventListener("click", changeFullview);
-
-/*
-  ⚠️ ЗАДАНИЕ ПОВЫШЕННОЙ СЛОЖНОСТИ - ВЫПОЛНЯТЬ ПО ЖЕЛАНИЮ
+ function addImg() {
+    galleryItems.forEach(function(item) {
+      const liImg = document.createElement("li");
+      preview.appendChild(liImg);
+      const img = document.createElement("img");
+      liImg.appendChild(img);
+      img.setAttribute("src", item.preview);
+      img.setAttribute("data-fullview", item.fullview);
+      img.setAttribute("alt", item.alt);
+    });
+  }
+  addImg();
   
-  Создайте плагин галлереи используя ES6 класс. Добавьте поля и методы класса так, 
-  чтобы можно было создать любое количество галлерей на странице. Функционал плагина 
-  аналогичный заданию выше.
+  function changeView(event) {
+    const target = event.target;
+    if (target.nodeName !== "IMG") return;
+    general(target);
+    opacityPreview(target);
+  }  
   
-  При создании экземпляра конструктор получает:
-    - items - список элементов для preview
-    - parentNode - ссылку на DOM-узел в который будут помещены fullview и preview
-    - defaultActiveItem - номер активного элемента preview по умолчанию
-    
-  Тогда создание экземпляра будет выглядеть следующим образом.
-*/
-
-new Gallery({
-   items: galleryItems,
-   parentNode: document.querySelector('.image-gallery'),
-   defaultActiveItem: 1
- });
- 
- /* Далее плагин работает в автономном режиме */
- class Gallery {
-    constructor (items,parentNode, defaultActiveItem){
-       this.item = items.map(item.preview);
-       this.parentNode = parentNode;
-       this.defaultActiveItem = [0];
+  function general(elem) {
+    fullviewImg.src = elem.dataset.fullview;
+  }
+  
+  function opacityPreview(elem) {
+    const currentElem = document.querySelector(".previewClass");
+    if (currentElem) {
+      currentElem.classList.remove("previewClass");
     }
- }
-//  - При клике в элемент preview, необходимо подменить src тега img внутри fullview
-//         на url из data-атрибута выбраного элемента.
+    elem.classList.add("previewClass");
+  }
 
-get InsertGallery (){
-   let addImage = this.items.reduce(pic, i) => pic +  `<>`
-}
+previewGallery.addEventListener("click", changeView);
+      
